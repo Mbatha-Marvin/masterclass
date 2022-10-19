49e5110f9f51f734b452e16f8972a24c1d294061
Mbatha-Marvin <mbathamarvin597@gmail.com>
2022-10-19 12:29:08 +0300

    created an error handler midleware

--- Express_app_with_mongoDB/backend/server.js
+++ Express_app_with_mongoDB/backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port  = process.env.PORT || 8080
 
@@ -12,5 +13,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/movies', require('./routes/movieRoutes'))
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`server is running on port ${port}`))
